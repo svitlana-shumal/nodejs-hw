8ad809b3759cb0907a43b18e47f8cdc74d366895
Svitlana Shumal <svetlankachajka@gmail.com>
2025-09-24 10:40:10 +0300

    fixed

--- src/server.js
+++ src/server.js
@@ -8,7 +8,22 @@ const PORT = process.env.PORT || 3030;
 
 app.use(express.json());
 app.use(cors());
-app.use(pino());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
 
 app.get('/notes', (req, res) => {
   res.status(200).json({ message: 'Retrieved all notes' });
