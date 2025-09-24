ee11f4b575dad0ea3e6d7af4b372a31f5ae01469
Svitlana Shumal <svetlankachajka@gmail.com>
2025-09-24 10:43:26 +0300

    fixed port

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import pino from 'pino-http';
 import 'dotenv/config';
 
 const app = express();
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT ?? 3030;
 
 app.use(express.json());
 app.use(cors());
