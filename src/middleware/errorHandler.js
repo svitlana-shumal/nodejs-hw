0afabada2ff0a28b11a73514d45430c203ee8473
Svitlana Shumal <svetlankachajka@gmail.com>
2025-09-27 13:51:15 +0300

    Set up MongoDB connection and project structure

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,6 @@
+export const errorHandler = (err, req, res, next) => {
+  console.error(err.message);
+  res.status(500).json({
+    message: err.message,
+  });
+};
---
+++ src/models/note.js
@@ -0,0 +1,39 @@
+import { Schema } from 'mongoose';
+import { model } from 'mongoose';
+
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    tag: {
+      type: String,
+      required: true,
+      enum: [
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      ],
+      default: 'Todo',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Note = model('Note', noteSchema);
