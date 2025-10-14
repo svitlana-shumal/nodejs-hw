52f5efaf06c86ec7d9393c028b3c7e40e8612f52
Svitlana Shumal <svetlankachajka@gmail.com>
2025-10-14 19:31:32 +0300

    Added 05 tasks

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,12 +3,16 @@ import { celebrate } from 'celebrate';
 import {
   loginUserSchema,
   registerUserSchema,
+  requestResetEmailSchema,
+  resetPasswordSchema,
 } from '../validations/authValidation.js';
 import {
   loginUser,
   logoutUser,
   refreshUserSession,
   registerUser,
+  requestResetEmail,
+  resetPassword,
 } from '../controllers/authController.js';
 
 const router = Router();
@@ -21,4 +25,15 @@ router.post('/auth/logout', logoutUser);
 
 router.post('/auth/refresh', refreshUserSession);
 
+router.post(
+  '/auth/request-reset-email',
+  celebrate(requestResetEmailSchema),
+  requestResetEmail,
+);
+
+router.post(
+  '/auth/reset-password',
+  celebrate(resetPasswordSchema),
+  resetPassword,
+);
 export default router;
--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 import authRoutes from './routes/authRoutes.js';
 import notesRoutes from './routes/notesRoutes.js';
 import cookieParser from 'cookie-parser';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -27,6 +28,7 @@ app.use(cookieParser());
 
 app.use(authRoutes);
 app.use(notesRoutes);
+app.use(userRoutes);
 app.use(notFoundHandler);
 app.use(errors());
 app.use(errorHandler);
